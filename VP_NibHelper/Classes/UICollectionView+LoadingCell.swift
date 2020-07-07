548f6c57ec2e6eaa7eb082065bd70b21b6deef7a
vukasin-popovic <67900667+vukasin-popovic@users.noreply.github.com>
2020-07-07 13:39:45 +0200

    Update UICollectionView+LoadingCell.swift

--- VP_NibHelper/Classes/UICollectionView+LoadingCell.swift
+++ VP_NibHelper/Classes/UICollectionView+LoadingCell.swift
@@ -10,15 +10,15 @@ import UIKit
 
 extension UICollectionView {
 
-    func registerClass<T: UICollectionViewCell>(_: T.Type) {
+    public func registerClass<T: UICollectionViewCell>(_: T.Type) {
         register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
 
-    func registerNib<T: UICollectionViewCell>(_: T.Type) {
+    public func registerNib<T: UICollectionViewCell>(_: T.Type) {
         register(T.nib, forCellWithReuseIdentifier: T.reuseIdentifier)
     }
 
-    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T {
+    public func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T {
         guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
             fatalError("Dequeing a cell with identifier: \(T.reuseIdentifier) failed.")
         }
